8442c41817f74bd0e9f18d689f3e3b7352ef628f
abdullah <a.ismail@mailbox.org>
2022-03-08 12:00:11 +0100

    deleted header without content

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -2,13 +2,14 @@ import { Col, Container, Row } from "react-bootstrap";
 
 export default function Header (){
     return(
-        <Container>
-            <Row>
-                <Col className="justify-content-center">
-                <h1 className="m-4">ToDos App</h1>
-                </Col>
-                <Col><h1 className="m-4"></h1></Col>
-            </Row>
-        </Container>
+        <div>
+            <Container>
+                <Row>
+                    <Col className="justify-content-center">
+                    <h1 className="m-4">ToDos App</h1>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
