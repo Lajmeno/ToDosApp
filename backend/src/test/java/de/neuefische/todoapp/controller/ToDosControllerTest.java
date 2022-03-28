5403e629f8552527d35a4fe71542b01bc9c942c1
abdullah <a.ismail@mailbox.org>
2022-03-28 11:27:46 +0200

    changed application.properties for tests

--- backend/src/test/java/de/neuefische/todoapp/controller/ToDosControllerTest.java
+++ backend/src/test/java/de/neuefische/todoapp/controller/ToDosControllerTest.java
@@ -22,10 +22,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class ToDosControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
+
     @Test
     void shouldCreateNewTestUser(){
         ResponseEntity<UserDocument> createResponse = restTemplate.postForEntity("/users", new UserDocument("test@email.com", "test", "test", "USER", null), UserDocument.class);
         assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
 
         ResponseEntity<String> loginResponse = restTemplate.postForEntity("/auth/login", new LoginData("test@email.com", "test"), String.class);
         assertThat(loginResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -37,6 +39,7 @@ class ToDosControllerTest {
         ResponseEntity<List> getToDosResponse = restTemplate.exchange("/todos", HttpMethod.GET, new HttpEntity<>(createHeaders(loginResponse.getBody())), List.class);
         assertThat(getToDosResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 
+
     }
 
     HttpHeaders createHeaders(String token) {
@@ -46,29 +49,5 @@ class ToDosControllerTest {
         }};
     }
 
-
-    @Test
-    @Disabled
-    void expectToGetOneToDo(){
-        ToDo toDo = new ToDo();
-        toDo.setTitle("Rechnung Test");
-        restTemplate.postForEntity("/todos",toDo, ToDo[].class );
-        ResponseEntity<ToDo[]> response = restTemplate.getForEntity("/todos", ToDo[].class);
-        assertThat(response.getBody()).contains(toDo);
-    }
-
-    @Test
-    @Disabled
-    void expectToChangeStatusOf1ToDo(){
-        ToDo toDo = new ToDo();
-        toDo.setTitle("Einkauf");
-        ToDo toDo2 = new ToDo("Pizza");
-        toDo2.setStatus(Status.INPROGRESS);
-        restTemplate.postForEntity("/todos", toDo, ToDo[].class );
-        restTemplate.put("/todos/" + toDo.getId(), toDo2);
-        ResponseEntity<ToDo[]> response = restTemplate.getForEntity("/todos", ToDo[].class);
-
-        assertEquals(Status.INPROGRESS, response.getBody()[0].getStatus());
-    }
 
 }
--- backend/src/test/java/de/neuefische/todoapp/service/ToDosServiceTest.java
+++ backend/src/test/java/de/neuefische/todoapp/service/ToDosServiceTest.java
@@ -15,19 +15,17 @@ import static org.mockito.Mockito.when;
 class ToDosServiceTest {
 
     @Test
-    @Disabled
     void shouldAdd1ToDo(){
         ToDosRepo mockRepo = mock(ToDosRepo.class);
         ToDosService service = new ToDosService(mockRepo);
         ToDo toDo = new ToDo("Aufraeumen");
         service.addToDo(toDo);
-        when(mockRepo.findAllByCreatedByOrderByStatus("test@mail.com")).thenReturn(List.of(toDo));
+        when(mockRepo.findAllByCreatedBy("test@mail.com")).thenReturn(List.of(toDo));
 
         assertEquals(List.of(toDo), service.getTodos("test@mail.com"));
     }
 
     @Test
-    @Disabled
     void expectToChangeStatusToInprogress(){
         ToDosRepo mockRepo = mock(ToDosRepo.class);
         ToDosService service = new ToDosService(mockRepo);
@@ -41,11 +39,10 @@ class ToDosServiceTest {
         when(mockRepo.findAllByCreatedBy("test@mail.com")).thenReturn(List.of(toDo, toDo2));
         service.changeToDoStatus(toDo.getId(), toDo2, "test@mail.com");
 
-        assertTrue(service.getTodos("test@mail.com").get(0).getStatus().equals(Status.INPROGRESS));
+        assertTrue(service.getTodos("test@mail.com").get(1).getStatus().equals(Status.INPROGRESS));
     }
 
     @Test
-    @Disabled
     void expectToRemove1ToDo(){
         ToDosRepo mockRepo = mock(ToDosRepo.class);
         ToDosService service = new ToDosService(mockRepo);
@@ -55,27 +52,9 @@ class ToDosServiceTest {
         service.addToDo(toDo2);
         when(mockRepo.findAllByCreatedBy("test@mail.com")).thenReturn(List.of(toDo1, toDo2));
         service.removeToDo(toDo1.getId(), "test@mail.com");
-        when(mockRepo.findAll()).thenReturn(List.of(toDo2));
+        when(mockRepo.findAllByCreatedBy("test@mail.com")).thenReturn(List.of(toDo2));
         assertEquals(List.of(toDo2), service.getTodos("test@mail.com"));
     }
 
-    @Test
-    @Disabled
-    void expectsToRemoveAllDoneToDos(){
-        ToDosRepo mockRepo = mock(ToDosRepo.class);
-        ToDosService service = new ToDosService(mockRepo);
-        ToDo toDo1 = new ToDo("Stromrechnung");
-        ToDo toDo2 = new ToDo("Einkauf");
-        ToDo toDo3 = new ToDo("Never Done");
-        toDo1.setStatus(Status.DONE);
-        toDo2.setStatus(Status.DONE);
-        service.addToDo(toDo1);
-        service.addToDo(toDo2);
-        service.addToDo(toDo3);
-
-        service.removeDoneToDos("test@mail.com");
-
-        assertEquals(List.of(toDo3), service.getTodos("test@mail.com"));
-    }
 
 }
